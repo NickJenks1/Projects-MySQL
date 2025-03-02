5ee31ad983a9f8f635089618681231d67a44daed
Nick Jenks <dtnjenks@gmail.com>
2025-03-01 21:49:13 -0500

    week 11

--- src/main/resources/projects-schema.sql
+++ src/main/resources/projects-schema.sql
@@ -28,7 +28,7 @@ CREATE TABLE material(
 	material_name VARCHAR(128) NOT NULL,
 	num_required INT,
 	cost DECIMAL(7,2),
-	FOREIGN KEY (project_id) REFERENCES project(project_id),
+	FOREIGN KEY (project_id) REFERENCES project(project_id) ON DELETE CASCADE,
 	PRIMARY KEY (material_id)
 );
 
@@ -37,14 +37,24 @@ CREATE TABLE step(
 	project_id INT NOT NULL,
 	step_text TEXT NOT NULL,
 	step_order INT NOT NULL,
-	FOREIGN KEY (project_id) REFERENCES project(project_id),
+	FOREIGN KEY (project_id) REFERENCES project(project_id) ON DELETE CASCADE,
 	PRIMARY KEY (step_id)
 );
 
 CREATE TABLE project_category(
 	project_id INT NOT NULL,
 	category_id INT NOT NULL,
-	FOREIGN KEY (project_id) REFERENCES project(project_id),
-	FOREIGN KEY (category_id) REFERENCES category(category_id),
+	FOREIGN KEY (project_id) REFERENCES project(project_id) ON DELETE CASCADE,
+	FOREIGN KEY (category_id) REFERENCES category(category_id) ON DELETE CASCADE,
 	UNIQUE KEY (project_id, category_id)
 );
+
+INSERT INTO project (project_name, estimated_hours, actual_hours, difficulty, notes) VALUES ('Hang a door', 4, 3, 3, 'Use the door hangers from Home Depot')
+
+INSERT INTO material (project_id, material_name, num_required, cost) VALUES (1, '2-inch screws', 20, 1.09);
+
+INSERT INTO step (project_id, step_text, step_order) VALUES (1, 'Screw door hangers on the top and bottom of each side of the door frame', 1);
+
+INSERT INTO category (category_name) VALUES ('Repairs');
+
+INSERT INTO project_category (project_id, category_id) VALUES (1, 1);
